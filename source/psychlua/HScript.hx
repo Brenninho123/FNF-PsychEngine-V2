50ae43c3173bb3d08a6d19139396b16275b874c1
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2025-12-31 18:48:10 -0500

    Atualizar o HScript.hx

--- source/psychlua/HScript.hx
+++ source/psychlua/HScript.hx
@@ -1,6 +1,13 @@
 package psychlua;
 
 import flixel.FlxBasic;
+import flixel.FlxG;
+import flixel.util.FlxColor;
+import flixel.math.FlxMath;
+import flixel.util.FlxTimer;
+import flixel.tweens.FlxTween;
+import flixel.tweens.FlxEase;
+
 import objects.Character;
 import psychlua.LuaUtils;
 import psychlua.CustomSubstate;
@@ -15,50 +22,34 @@ import tea.SScript;
 class HScript extends SScript
 {
 	public var modFolder:String;
+	public var origin:String;
 
 	#if LUA_ALLOWED
 	public var parentLua:FunkinLua;
+	#end
 
 	public static function initHaxeModule(parent:FunkinLua)
 	{
-		if(parent.hscript == null)
-		{
-			trace('initializing haxe interp for: ${parent.scriptName}');
+		if (parent.hscript == null)
 			parent.hscript = new HScript(parent);
-		}
 	}
 
 	public static function initHaxeModuleCode(parent:FunkinLua, code:String, ?varsToBring:Any = null)
 	{
-		var hs:HScript = try parent.hscript catch (e) null;
-		if(hs == null)
-		{
-			trace('initializing haxe interp for: ${parent.scriptName}');
+		var hs:HScript = parent.hscript;
+		if (hs == null)
 			parent.hscript = new HScript(parent, code, varsToBring);
-		}
 		else
-		{
 			hs.doString(code);
-			@:privateAccess
-			if(hs.parsingException != null)
-			{
-				PlayState.instance.addTextToDebug(
-					'ERROR ON LOADING (${hs.origin}): ${hs.parsingException.message}',
-					FlxColor.RED
-				);
-			}
-		}
 	}
-	#end
 
-	public var origin:String;
 	var varsToBring:Any = null;
 
 	override public function new(?parent:Dynamic, ?file:String, ?varsToBring:Any = null)
 	{
 		if (file == null) file = '';
-
 		this.varsToBring = varsToBring;
+
 		super(file, false, false);
 
 		#if LUA_ALLOWED
@@ -70,17 +61,6 @@ class HScript extends SScript
 		}
 		#end
 
-		if (scriptFile != null && scriptFile.length > 0)
-		{
-			origin = scriptFile;
-			#if MODS_ALLOWED
-			var myFolder:Array<String> = scriptFile.split('/');
-			if(myFolder[0] + '/' == Paths.mods()
-				&& (Mods.currentModDirectory == myFolder[1] || Mods.getGlobalMods().contains(myFolder[1])))
-				modFolder = myFolder[1];
-			#end
-		}
-
 		preset();
 		execute();
 	}
@@ -89,98 +69,195 @@ class HScript extends SScript
 	{
 		super.preset();
 
-		set('FlxG', flixel.FlxG);
-		set('FlxMath', flixel.math.FlxMath);
-		set('FlxSprite', flixel.FlxSprite);
-		set('FlxCamera', flixel.FlxCamera);
-		set('PsychCamera', backend.PsychCamera);
-		set('FlxTimer', flixel.util.FlxTimer);
-		set('FlxTween', flixel.tweens.FlxTween);
-		set('FlxEase', flixel.tweens.FlxEase);
+		// ===============================
+		// CORE CLASSES
+		// ===============================
+		set('FlxG', FlxG);
+		set('FlxMath', FlxMath);
+		set('FlxTween', FlxTween);
+		set('FlxEase', FlxEase);
+		set('FlxTimer', FlxTimer);
 		set('FlxColor', CustomFlxColor);
-		set('Countdown', backend.BaseStage.Countdown);
+
 		set('PlayState', PlayState);
 		set('Paths', Paths);
-		set('StorageUtil', StorageUtil);
 		set('Conductor', Conductor);
 		set('ClientPrefs', ClientPrefs);
+
 		set('Character', Character);
-		set('Alphabet', Alphabet);
 		set('Note', objects.Note);
 		set('CustomSubstate', CustomSubstate);
 
-		#if (!flash && sys)
-		set('FlxRuntimeShader', flixel.addons.display.FlxRuntimeShader);
-		#end
-		set('ShaderFilter', openfl.filters.ShaderFilter);
-		set('StringTools', StringTools);
-
-		set('setVar', function(name:String, value:Dynamic) {
+		// ===============================
+		// VARIABLES
+		// ===============================
+		set('setVar', function(name:String, value:Dynamic)
+		{
 			PlayState.instance.variables.set(name, value);
 			return value;
 		});
-		set('getVar', function(name:String) {
-			return PlayState.instance.variables.get(name);
+
+		set('getVar', function(name:String)
+		{
+			return PlayState.instance.variables.exists(name)
+				? PlayState.instance.variables.get(name)
+				: null;
 		});
-		set('removeVar', function(name:String) {
+
+		set('removeVar', function(name:String)
+		{
 			return PlayState.instance.variables.remove(name);
 		});
 
-		set('debugPrint', function(text:String, ?color:FlxColor = null) {
-			if(color == null) color = FlxColor.WHITE;
+		// ===============================
+		// DEBUG
+		// ===============================
+		set('debugPrint', function(text:String, ?color:Int = null)
+		{
+			if (color == null) color = FlxColor.WHITE;
 			PlayState.instance.addTextToDebug(text, color);
 		});
 
-		set('getFPS', () -> Std.int(FlxG.drawFramerate));
-		set('getUpdateFPS', () -> Std.int(FlxG.updateFramerate));
-
+		// ===============================
+		// SONG / TIMING (CORRIGIDO)
+		// ===============================
 		set('getSongTime', () -> Conductor.songPosition);
 		set('getSongTimeSeconds', () -> Conductor.songPosition / 1000);
-		set('getBeat', () -> Std.int(Conductor.curBeat));
-		set('getStep', () -> Std.int(Conductor.curStep));
 
-		set('cameraShake', function(cam:String, intensity:Float, duration:Float) {
-			var c = (cam == 'hud') ? FlxG.cameras.list[1] : FlxG.camera;
-			if(c != null) c.shake(intensity, duration);
-		});
-		set('cameraFlash', function(cam:String, color:Int, duration:Float) {
-			var c = (cam == 'hud') ? FlxG.cameras.list[1] : FlxG.camera;
-			if(c != null) c.flash(color, duration);
-		});
-		set('cameraFade', function(cam:String, color:Int, duration:Float, ?fadeIn:Bool = false) {
-			var c = (cam == 'hud') ? FlxG.cameras.list[1] : FlxG.camera;
-			if(c != null) c.fade(color, duration, fadeIn);
+		set('getBeat', () ->
+		{
+			return PlayState.instance != null ? PlayState.instance.curBeat : 0;
 		});
 
-		set('tweenX', (obj, v, t) -> FlxTween.tween(obj, {x: v}, t));
-		set('tweenY', (obj, v, t) -> FlxTween.tween(obj, {y: v}, t));
-		set('tweenAlpha', (obj, v, t) -> FlxTween.tween(obj, {alpha: v}, t));
-		set('tweenAngle', (obj, v, t) -> FlxTween.tween(obj, {angle: v}, t));
+		set('getStep', () ->
+		{
+			return PlayState.instance != null ? PlayState.instance.curStep : 0;
+		});
 
-		set('randomInt', (a, b) -> FlxG.random.int(a, b));
-		set('randomFloat', (a, b) -> FlxG.random.float(a, b));
+		// ===============================
+		// PAUSE (SAFE)
+		// ===============================
+		set('isPaused', () ->
+		{
+			return PlayState.instance != null && PlayState.instance.paused;
+		});
 
-		set('isPaused', () -> PlayState.instance.paused);
-		set('pauseGame', () -> PlayState.instance.openPauseMenu());
-		set('resumeGame', () -> PlayState.instance.closeSubState());
+		set('pauseGame', () ->
+		{
+			if (PlayState.instance != null && !PlayState.instance.paused)
+			{
+				PlayState.instance.paused = true;
+				FlxG.sound.music.pause();
+			}
+		});
 
-		set('isMobile', () -> #if mobile true #else false #end);
-		set('isDesktop', () -> #if desktop true #else false #end);
+		set('resumeGame', () ->
+		{
+			if (PlayState.instance != null && PlayState.instance.paused)
+			{
+				PlayState.instance.paused = false;
+				FlxG.sound.music.resume();
+			}
+		});
 
-		#if mobile
-		set('vibrate', (ms:Int = 100) -> lime.ui.Haptic.vibrate(0, ms));
+		// ===============================
+		// CAMERA
+		// ===============================
+		set('cameraShake', function(intensity:Float, duration:Float)
+		{
+			FlxG.camera.shake(intensity, duration);
+		});
+
+		set('cameraFlash', function(color:Int, duration:Float)
+		{
+			FlxG.camera.flash(color, duration);
+		});
+
+		set('cameraFade', function(color:Int, duration:Float, ?fadeIn:Bool = false)
+		{
+			FlxG.camera.fade(color, duration, fadeIn);
+		});
+
+		// ===============================
+		// RANDOM / MATH
+		// ===============================
+		set('randomInt', (min:Int, max:Int) -> FlxG.random.int(min, max));
+		set('randomFloat', (min:Float, max:Float) -> FlxG.random.float(min, max));
+		set('lerp', (a:Float, b:Float, t:Float) -> FlxMath.lerp(a, b, t));
+
+		// ===============================
+		// PLATFORM
+		// ===============================
+		set('isMobile', () ->
+		{
+			#if mobile
+			return true;
+			#else
+			return false;
+			#end
+		});
+
+		// ===============================
+		// LUA LINK
+		// ===============================
+		#if LUA_ALLOWED
+		set('parentLua', parentLua);
+		#else
+		set('parentLua', null);
 		#end
 
 		set('this', this);
 		set('game', FlxG.state);
 
-		if(varsToBring != null)
+		// ===============================
+		// BRING VARS
+		// ===============================
+		if (varsToBring != null)
 		{
-			for (k in Reflect.fields(varsToBring))
-				set(k, Reflect.field(varsToBring, k));
+			for (key in Reflect.fields(varsToBring))
+				set(key, Reflect.field(varsToBring, key));
 			varsToBring = null;
 		}
 	}
+
+	public function executeCode(?func:String, ?args:Array<Dynamic>):TeaCall
+	{
+		if (func == null || !exists(func)) return null;
+		return call(func, args);
+	}
+
+	public function executeFunction(func:String, args:Array<Dynamic>):TeaCall
+	{
+		return call(func, args);
+	}
+
+	#if LUA_ALLOWED
+	public static function implement(funk:FunkinLua)
+	{
+		funk.addLocalCallback("runHaxeCode", function(code:String, ?vars:Any = null, ?func:String = null, ?args:Array<Dynamic> = null)
+		{
+			#if SScript
+			initHaxeModuleCode(funk, code, vars);
+			if (func != null)
+			{
+				var ret = funk.hscript.executeCode(func, args);
+				return ret != null ? ret.returnValue : null;
+			}
+			#end
+			return null;
+		});
+
+		funk.addLocalCallback("runHaxeFunction", function(func:String, ?args:Array<Dynamic> = null)
+		{
+			#if SScript
+			var ret = funk.hscript.executeFunction(func, args);
+			return ret != null ? ret.returnValue : null;
+			#else
+			return null;
+			#end
+		});
+	}
+	#end
 
 	override public function destroy()
 	{
@@ -196,8 +273,12 @@ class CustomFlxColor
 	public static var BLACK(default, null):Int = FlxColor.BLACK;
 	public static var WHITE(default, null):Int = FlxColor.WHITE;
 	public static var RED(default, null):Int = FlxColor.RED;
+	public static var GREEN(default, null):Int = FlxColor.GREEN;
+	public static var BLUE(default, null):Int = FlxColor.BLUE;
 
-	public static function fromRGB(r:Int,g:Int,b:Int,a:Int=255):Int
-		return cast FlxColor.fromRGB(r,g,b,a);
+	public static function fromRGB(r:Int, g:Int, b:Int, a:Int = 255):Int
+	{
+		return cast FlxColor.fromRGB(r, g, b, a);
+	}
 }
 #end
