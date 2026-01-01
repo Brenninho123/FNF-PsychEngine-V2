a965f2a2166313ed40a1acab7ce84d52188113a4
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2025-12-31 20:23:08 -0500

    Atualizar o MasterEditorMenu.hx

--- source/states/editors/MasterEditorMenu.hx
+++ source/states/editors/MasterEditorMenu.hx
@@ -1,7 +1,14 @@
 package states.editors;
 
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import flixel.group.FlxTypedGroup;
+import flixel.ui.FlxButton;
+
 import backend.WeekData;
-import objects.Character;
+import objects.Alphabet;
 import states.MainMenuState;
 import states.FreeplayState;
 
@@ -18,7 +25,6 @@ class MasterEditorMenu extends MusicBeatState
 	];
 	private var grpTexts:FlxTypedGroup<Alphabet>;
 	private var directories:Array<String> = [null];
-
 	private var curSelected = 0;
 	private var curDirectory = 0;
 	private var directoryTxt:FlxText;
@@ -26,10 +32,12 @@ class MasterEditorMenu extends MusicBeatState
 	override function create()
 	{
 		FlxG.camera.bgColor = FlxColor.BLACK;
+
 		#if DISCORD_ALLOWED
 		DiscordClient.changePresence("Editors Main Menu", null);
 		#end
 
+		// Background
 		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
 		bg.scrollFactor.set();
 		bg.color = 0xFF353535;
@@ -38,13 +46,10 @@ class MasterEditorMenu extends MusicBeatState
 		grpTexts = new FlxTypedGroup<Alphabet>();
 		add(grpTexts);
 
-		// CENTRALIZANDO O MENU
-		var startY:Float = 200;
-		var spacing:Float = 60;
+		// Adiciona opções
 		for (i in 0...options.length)
 		{
-			var textWidth:Float = Paths.fontWidth("vcr.ttf", options[i], 32); // largura aproximada
-			var leText:Alphabet = new Alphabet((FlxG.width - textWidth) / 2, startY + i * spacing, options[i], true);
+			var leText:Alphabet = new Alphabet(90, 320 + i * 40, options[i], true);
 			leText.isMenuItem = true;
 			leText.targetY = i;
 			grpTexts.add(leText);
@@ -52,19 +57,18 @@ class MasterEditorMenu extends MusicBeatState
 		}
 
 		#if MODS_ALLOWED
+		// Barra de diretório
 		var textBG:FlxSprite = new FlxSprite(0, FlxG.height - 42).makeGraphic(FlxG.width, 42, 0xFF000000);
 		textBG.alpha = 0.6;
 		add(textBG);
 
-		directoryTxt = new FlxText(textBG.x, textBG.y + 4, FlxG.width, '', 32);
-		directoryTxt.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER);
+		directoryTxt = new FlxText(0, textBG.y + 4, FlxG.width, '', 24);
+		directoryTxt.setFormat(Paths.font("vcr.ttf"), 24, FlxColor.WHITE, "center");
 		directoryTxt.scrollFactor.set();
 		add(directoryTxt);
 
 		for (folder in Mods.getModDirectories())
-		{
 			directories.push(folder);
-		}
 
 		var found:Int = directories.indexOf(Mods.currentModDirectory);
 		if(found > -1) curDirectory = found;
@@ -96,35 +100,18 @@ class MasterEditorMenu extends MusicBeatState
 		#end
 
 		if (controls.BACK)
-		{
 			MusicBeatState.switchState(new MainMenuState());
-		}
 
 		if (controls.ACCEPT)
-		{
-			switch(options[curSelected])
-			{
-				case 'Chart Editor': LoadingState.loadAndSwitchState(new ChartingState(), false);
-				case 'Character Editor': LoadingState.loadAndSwitchState(new CharacterEditorState(Character.DEFAULT_CHARACTER, false));
-				case 'Week Editor': MusicBeatState.switchState(new WeekEditorState());
-				case 'Menu Character Editor': MusicBeatState.switchState(new MenuCharacterEditorState());
-				case 'Dialogue Editor': LoadingState.loadAndSwitchState(new DialogueEditorState(), false);
-				case 'Dialogue Portrait Editor': LoadingState.loadAndSwitchState(new DialogueCharacterEditorState(), false);
-				case 'Note Splash Debug': MusicBeatState.switchState(new NoteSplashDebugState());
-			}
-			FlxG.sound.music.volume = 0;
-			FreeplayState.destroyFreeplayVocals();
-		}
+			activateOption();
 
-		// Atualiza a posição vertical e alfa dos itens
-		var bullShit:Int = 0;
+		// Atualiza posição e alpha do texto
+		var index:Int = 0;
 		for (item in grpTexts.members)
 		{
-			item.targetY = bullShit - curSelected;
-			bullShit++;
-
-			item.alpha = 0.6;
-			if (item.targetY == 0) item.alpha = 1;
+			item.targetY = index - curSelected;
+			item.alpha = (item.targetY == 0) ? 1 : 0.6;
+			index++;
 		}
 
 		super.update(elapsed);
@@ -133,9 +120,7 @@ class MasterEditorMenu extends MusicBeatState
 	function changeSelection(change:Int = 0)
 	{
 		FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
-
 		curSelected += change;
-
 		if (curSelected < 0) curSelected = options.length - 1;
 		if (curSelected >= options.length) curSelected = 0;
 	}
@@ -146,21 +131,42 @@ class MasterEditorMenu extends MusicBeatState
 		FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
 
 		curDirectory += change;
-
-		if(curDirectory < 0) curDirectory = directories.length - 1;
-		if(curDirectory >= directories.length) curDirectory = 0;
+		if (curDirectory < 0) curDirectory = directories.length - 1;
+		if (curDirectory >= directories.length) curDirectory = 0;
 
 		WeekData.setDirectoryFromWeek();
 
-		if(directories[curDirectory] == null || directories[curDirectory].length < 1)
-			directoryTxt.text = '< No Mod Directory Loaded >';
+		if (directories[curDirectory] == null || directories[curDirectory].length < 1)
+			directoryTxt.text = '< NO MOD DIRECTORY LOADED >';
 		else
 		{
 			Mods.currentModDirectory = directories[curDirectory];
-			directoryTxt.text = '< Loaded Mod Directory: ' + Mods.currentModDirectory + ' >';
+			directoryTxt.text = '< LOADED MOD DIRECTORY: ' + Mods.currentModDirectory + ' >';
 		}
-
 		directoryTxt.text = directoryTxt.text.toUpperCase();
 	}
 	#end
+
+	function activateOption()
+	{
+		switch(options[curSelected])
+		{
+			case 'Chart Editor':
+				LoadingState.loadAndSwitchState(new ChartingState(), false);
+			case 'Character Editor':
+				LoadingState.loadAndSwitchState(new CharacterEditorState(Character.DEFAULT_CHARACTER, false));
+			case 'Week Editor':
+				MusicBeatState.switchState(new WeekEditorState());
+			case 'Menu Character Editor':
+				MusicBeatState.switchState(new MenuCharacterEditorState());
+			case 'Dialogue Editor':
+				LoadingState.loadAndSwitchState(new DialogueEditorState(), false);
+			case 'Dialogue Portrait Editor':
+				LoadingState.loadAndSwitchState(new DialogueCharacterEditorState(), false);
+			case 'Note Splash Debug':
+				MusicBeatState.switchState(new NoteSplashDebugState());
+		}
+		FlxG.sound.music.volume = 0;
+		FreeplayState.destroyFreeplayVocals();
+	}
 }
